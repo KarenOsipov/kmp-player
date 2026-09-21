0adb69e6a029a5008de272ddc2324bbb9ffd606f
KarenOsipov <karenosipov@gmail.com>
2026-09-21 12:39:35 +0500

    update2

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // доступ защищён правилами (RLS) в schema.sql.
 window.KMP_CONFIG = {
   supabaseUrl: 'https://nejlpzqtokrgvzsgqsud.supabase.co/rest',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lamxwenF0b2tyZ3Z6c2dxc3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5NjQ4MTQsImV4cCI6MjEwNTU0MDgxNH0.5yTXtGi7_85U6wLnXm3VBBlJrVAkxGUCIN1mkReo0rI'
+  supabaseKey: 'sb_publishable_lmLGtezi77Qvrfb4CIqcGg_Y0lN9iFL'
 };
