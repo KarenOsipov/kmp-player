6434456fd2b0fcee5787f637d88e20bbda1bb775
KarenOsipov <karenosipov@gmail.com>
2026-09-24 16:28:01 +0500

    Минимальный вид, фикс истории и комнат

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // KMP Player service worker: страница, скрипты и config.js всегда свежие, картинки и шрифты из кеша.
-const V = 'kmp-v24';
+const V = 'kmp-v25';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './vendor/supabase.js', './demos/d1.mp3', './icons/icon-192.png', './icons/icon-512.png', './icons/favicon.svg', './icons/apple-touch-icon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(V).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
